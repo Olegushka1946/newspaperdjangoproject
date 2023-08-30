b250d2e88e6768e142ca8913c80f967897555c63
Olegushka1946 <126393443+Olegushka1946@users.noreply.github.com>
2023-08-30 23:18:08 +0300

    Add files via upload

---
+++ newspaper/news/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
+# Create your views here.
+
+
+class NewsList(ListView):
+    # Указываем модель, объекты которой мы будем выводить
+    model = Post
+    # Поле, которое будет использоваться для сортировки объектов
+    ordering = '-time_in'
+    # Указываем имя шаблона, в котором будут все инструкции о том,
+    # как именно пользователю должны быть показаны наши объекты
+    template_name = 'News.html'
+    # Это имя списка, в котором будут лежать все объекты.
+    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
+    context_object_name = 'news'
+
+
+class PostDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — post.html
+    template_name = 'Post.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'
